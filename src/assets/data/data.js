58b5a9aa760466c07dc4b460f926298e42c771b6
kiranmaitarlada <109078229+kiranmaitarlada@users.noreply.github.com>
2025-04-12 15:21:44 +0000

    Created website

---
+++ src/assets/data/data.js
@@ -0,0 +1,98 @@
+export const data = {
+    bride: {
+        L: {
+            id: 1,
+            name: 'Vineeth Kumar',
+            child: 'Son of',
+            father: 'Aravala Latchunnaidu',
+            mother: 'Lakshmi Devi',
+            image: './src/assets/images/cowo.png'
+        },
+        P: {
+            id: 2,
+            name: 'Kiranmai',
+            child: 'Daughter of ',
+            father: 'Tarlada Vijaya bhaskara rao',
+            mother: 'Leela Rani',
+            image: './src/assets/images/cewe.png'
+        },
+
+        couple: './src/assets/images/couple.png'
+    },
+
+    time: {
+        marriage: {
+            year: '2025',
+            month: 'April',
+            date: '20',
+            day: 'Sunday',
+            hours: {
+                start: '08.29',
+            }
+        },
+        address: 'Sasi Conventions, Rice Millers Association, Opposite to Kinnera Theatre, Srikakulam.'
+    },
+
+    link: {
+        calendar: 'https://calendar.app.google/VHXV4NTTwcjsEzT56',
+        map: 'https://maps.app.goo.gl/Cqw9svLFWadkWk467',
+    },
+
+    gallery: [
+        {
+            id: 1,
+            image: './src/assets/images/image1.png'
+        },
+        {
+            id: 2,
+            image: './src/assets/images/image2.png'
+        },
+        {
+            id: 3,
+            image: './src/assets/images/image3.png'
+        },
+        {
+            id: 4,
+            image: './src/assets/images/image4.png'
+        },
+        {
+            id: 5,
+            image: './src/assets/images/image5.png'
+        },
+        {
+            id: 6,
+            image: './src/assets/images/image6.png'
+        }
+    ],
+
+    audio: './src/assets/audio/wedding.mp3',
+
+    api: 'https://script.google.com/macros/s/AKfycbyydz6N4p2VWUG8zsXeURv6ap9RP8a4eC3x6N3x6qTDjMVr1cIBz9S0NsHw2rWvBOSXGg/exec',
+
+    navbar: [
+        {
+            id: 1,
+            teks: 'Home',
+            icon: 'bx bxs-home-heart',
+            path: '#home',
+        },
+        {
+            id: 2,
+            teks: 'Bride',
+            icon: 'bx bxs-group',
+            path: '#bride',
+        },
+        {
+            id: 3,
+            teks: 'Calender',
+            icon: 'bx bxs-calendar-check',
+            path: '#time',
+        },
+        {
+            id: 4,
+            teks: 'Gallery',
+            icon: 'bx bxs-photo-album',
+            path: '#gallery',
+        },
+    ],
+}
